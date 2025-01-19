1038d3235a9bb208f7d7c06d4b939c4d2167a25f
bob <hack_engine@outlook.it>
2025-01-19 14:29:40 +0100

    add echo message Unlock repo

--- utils/unlock-repo.sh
+++ utils/unlock-repo.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo "Unlock repo Mega"
 
 docker run --rm -e RESTIC_HOSTNAME="Mondo" \
            -e RESTIC_PASSWORD=minecraft \
@@ -7,3 +8,5 @@ docker run --rm -e RESTIC_HOSTNAME="Mondo" \
            -v ./rclone.conf:/root/.config/rclone/rclone.conf \
            --network default \
            docker.io/tofran/restic-rclone:0.17.0_1.68.2 -r rclone:mega:/backups unlock --remove-all
+
+echo "Unlock repo Mega completed."
